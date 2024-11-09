091d9f12eb3806c29e847c18ecdea25a446d9445
eason <eason0326@gmail.com>
2024-11-09 16:24:15 +0800

    docker compose scrpit

--- main.sh
+++ main.sh
@@ -2,15 +2,34 @@
 
 # name='Eason'
 
-read -p "Enter your branch:" branch
+#read -p "Enter your branch:" branch
 
-if [ "$branch" == "main" ] ||  [ "$branch" == "master" ] ; then
-    echo "this is prod ${branch}"
-else 
-    echo "this is dev"
+#if [ "$branch" == "main" ] ||  [ "$branch" == "master" ] ; then
+#    echo "this is prod ${branch}"
+#else 
+#    echo "this is dev"
 
-fi
+#fi
 
 #echo "hello ${name}"
 
+#if [-f a.txt]; then 
+#    echo "a.txt exists"
+#else    
+#    echo "a.txt not exists"
+
+#fi
+
+
+
+#for i in `ls`; do
+#    echo $i
+
+#done
+
+##test
+
+
+
 read -n 1 -s
+
---
+++ main2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function hello() {
+    echo "hello $1 $2" #參數1 參數2
+
+}
+
+hello "Amy" "OKOK"
+hello "Eason"
+
+
+read -n 1 -s
